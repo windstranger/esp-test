f7579ad486a31d42e35e21abbdbc9692e797288a
akondratov <akondratov@it-one.ru>
2025-01-02 06:06:19 +0300

    initial commit, simple structure modification structure,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,127 @@
-import Image from "next/image";
+'use client'
+
+import {ChangeEvent, memo, useCallback, useEffect, useRef, useState} from "react";
+import {useAtomValue, useSetAtom} from "jotai";
+import {jsonAtom} from "@/components/pages/main/atoms";
+import {JSONData, User} from "@/models/User";
+import {debounce} from "next/dist/server/utils";
+
+function convertArrayToObject(array: User[]): JSONData {
+    const ids: number[] = [];
+    const data: JSONData["data"] = {};
+
+    array.forEach((item, index) => {
+        const randomId = index // Generate a random ID
+        ids.push(randomId);
+        data[randomId] = item;
+    });
+
+    return {ids, data};
+}
+
+function FilePicker() {
+    const setJSONData = useSetAtom(jsonAtom);
+    const [isLoading, setIsLoading] = useState<boolean>()
+    const fileProcessor = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        setIsLoading(true);
+        const file = event.target?.files?.[0]; // Get the selected file
+        if (!file) {
+            alert('No file selected!');
+            return;
+        }
+
+        const reader = new FileReader(); // Create a FileReader instance
+
+        // Define the onload event to parse and display the JSON
+        reader.onload = function (e: ProgressEvent<FileReader>) {
+            try {
+                if (e.target?.result && typeof e.target?.result === 'string') {
+                    const json = JSON.parse(e.target.result); // Parse JSON content
+                    const data = convertArrayToObject(json)
+                    setJSONData(data)
+                }
+            } catch (error: unknown) {
+                if (error instanceof Error) {
+                    console.log('Invalid JSON file!', error.message);
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        reader.onerror = () => {
+            setIsLoading(false);
+        }
+
+        reader.readAsText(file);
+
+    }, [setJSONData]);
+
+    return <div>
+        {isLoading && "Loading..."}
+        <input type="file" onChange={fileProcessor} accept={"application/json"}/>
+    </div>
+}
+
+const JSONDataRenderer = memo(function JSONDataRenderer({data, el}: { data: User, el: number }) {
+    console.log("rerenders");
+    const inputRef = useRef<HTMLInputElement>(null);
+    const setJSONData = useSetAtom(jsonAtom);
+    useEffect(() => {
+        const inputElement = inputRef.current;
+        if (inputElement) {
+            const handleChange = debounce((event: Event) => {
+                const target = event.target as HTMLInputElement;
+                console.log("Input value changed to:", target.value);
+                setJSONData(prev => {
+                    if (prev) {
+                        prev.data[el] = {...prev.data[el], age: Number(target.value)}
+                        const res = {...prev, data: {...prev.data}};
+                        return res;
+                    }
+                });
+            }, 500);
+
+            inputElement.addEventListener("input", handleChange);
+
+            // Cleanup event listener on component unmount
+            return () => {
+                inputElement.removeEventListener("input", handleChange);
+            };
+        }
+    }, [setJSONData]);
+
+
+    return <div className={"flex flex-col bg-gray-200 p-4 shadow"}>
+        <div className={"grid grid-cols-2 justify-between"}>
+            <div>name:</div>
+            <div>{data.name}</div>
+        </div>
+        <div className={"grid grid-cols-2 justify-between"}>
+            <div>age:</div>
+            <input ref={inputRef} defaultValue={data.age} type={"text"}/>
+        </div>
+    </div>;
+});
+
+function JSONArrayRenderer() {
+    const jsonData = useAtomValue(jsonAtom)
+    return <div className={"flex flex-col gap-4 max-w-[320px]"}>{jsonData?.ids.map(el => {
+        return <JSONDataRenderer key={el} data={jsonData?.data[el]} el={el}/>
+    })}</div>
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const jsonData = useAtomValue(jsonAtom);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div className="">
+            <button onClick={() => {
+                console.log(jsonData);
+            }}>download modified json
+            </button>
+            <FilePicker/>
+            <JSONArrayRenderer/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
---
+++ src/components/pages/main/atoms.ts
@@ -0,0 +1,5 @@
+import {atom} from "jotai";
+import {JSONData} from "@/models/User";
+
+export const jsonAtom = atom<JSONData>();
+
